542ad2c688bbd41cb4d79b75587d0aebf2d1e167
Matías Roy Rodriguez <matiasrr980@gmail.com>
2022-11-21 14:58:39 -0300

    feat: delete notes

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,8 +1,9 @@
-import { collection, doc, setDoc } from 'firebase/firestore/lite'
+import { async } from '@firebase/util';
+import { collection, deleteDoc, doc, setDoc } from 'firebase/firestore/lite'
 import { FirebaseDB } from '../../firebase/config';
 import { fileUpload } from '../../helpers/fileUpload';
 import { loadNotes } from '../../helpers/loadNotes';
-import { addNewEmptyNote, setActiveNote, savingNewNote, setNotes, updateNote, setSaving, setPhotosToActiveNote } from './';
+import { addNewEmptyNote, deleteNoteById, setActiveNote, savingNewNote, setNotes, updateNote, setSaving, setPhotosToActiveNote } from './';
 
 export const startNewNote = () => {
 	return async (dispatch, getState) => {
@@ -74,6 +75,19 @@ export const startUploadingFiles = (files = []) => {
 
 		const photosUrls = await Promise.all(fileUploadPromises);
 		dispatch(setPhotosToActiveNote(photosUrls));
+	}
+
+}
+
+export const startDeletingNote = () => {
+	return async (dispatch, getState) => {
+		const { uid } = getState().auth;
+		const { active: note } = getState().journal;
+
+		const docRef = doc(FirebaseDB, `${uid}/journal/notes/${note.id}`);
+		await deleteDoc(docRef);
+
+		dispatch(deleteNoteById(note.id));
 	}
 
 }
